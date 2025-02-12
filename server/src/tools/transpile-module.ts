b465dd05db8210841c15e66bfb4f89190fa1043d
maejimafumika <maejima-fumika075@g.ecc.u-tokyo.ac.jp>
2025-02-12 11:43:04 +0900

    Add spi ffi.

--- server/src/tools/transpile-module.ts
+++ server/src/tools/transpile-module.ts
@@ -7,13 +7,28 @@ import Session from "../server/session";
 
 const cProlog = `
 #include <stdint.h>
-#include "../${FILE_PATH.C_RUNTIME_H}"
-#include "../${FILE_PATH.PROFILER_H}"
+#include "c-runtime.h"
 `
 
+const modules: Map<string, GlobalVariableNameTable> = new Map();
+let globalNameTable: GlobalVariableNameTable|undefined = undefined;
+
 function main(moduleName: string) {
+  const importer = (fname: string) => {
+    const mod = modules.get(fname);
+    if (mod)
+      return mod;
+    else {
+      const ffi = fs.readFileSync(`${FILE_PATH.MODULES}/${fname}/${fname}.bs`).toString();
+      const moduleId = Session.moduleNameToId(fname);
+      const result = transpile(0, ffi, globalNameTable, importer, moduleId);
+      modules.set(fname, result.names);
+      return result.names;
+    }
+  }
+
   const src = fs.readFileSync(`${FILE_PATH.MODULES}/${moduleName}/${moduleName}.bs`).toString()
-  const result = transpile(0, src, undefined, undefined, Session.moduleNameToId(moduleName)).code
+  const result = transpile(0, src, globalNameTable, importer, Session.moduleNameToId(moduleName)).code
   fs.writeFileSync(`${FILE_PATH.MODULES}/${moduleName}/${moduleName}.c`, cProlog + result)
   console.log('done')
 }
